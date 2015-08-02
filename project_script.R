a5882ca0874d60f32dd3e3a792375e0548777a9d
Rose Nyameke <neorzay.01@gmail.com>
2015-08-02 19:27:41 -0400

    data split

--- project_script.R
+++ project_script.R
@@ -1,12 +1,29 @@
-data$Total.Attempts <- data$Attempts.Open.Play.on.target + data$Attempts.Open.Play.off.target + data$Attempts.from.Throws.on.target
+#read inital data file
+read.csv("Premier League 2011-12 Match by Match.csv")
 
-###
-#summing total attempts
+#aggregating total attempts
 data$Total.Attempts <- rowSums(data[, c(64:75)])
+
+#choosing variables for analysis
 sub_data<- subset(data, select = c("Date", "Player.ID", "Team", "Team.Id", "Opposition.id", "Venue",
                                    "Goals", "Shots.On.Target.inc.goals", "Blocked.Shots","Total.Successful.Passes.All",
                                    "Total.Unsuccessful.Passes.All", "Total.Attempts",
                                    "Key.Passes"))
+
+#storing the file with desired variables
+write.csv("sub_data", "small_soccer_data.csv", row.names = F)
+
+
+#####################################################################################################################
+
+#split data set in two
+n <- nrow(sub_data)
+half.point <- round(nrow(sub_data)/2)
+
+train.data <- sub_data[1:half.point, ]
+test.data <- sub_data[(half.point+1):n, ]
+
+
 
 #one year's worth of very detailed data, down to the individual. We plan on aggregating this for team stats
 #multiple years' worth of match data, with only information about goals and home vs. away
