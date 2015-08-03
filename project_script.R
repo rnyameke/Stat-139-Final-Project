b501fb3f711a17aa9374a1916831ed36b5e13447
Rose Nyameke <neorzay.01@gmail.com>
2015-08-02 21:48:26 -0400

    tidying step 1

--- project_script.R
+++ project_script.R
@@ -5,23 +5,32 @@ read.csv("Premier League 2011-12 Match by Match.csv")
 data$Total.Attempts <- rowSums(data[, c(64:75)])
 
 #choosing variables for analysis
-sub_data<- subset(data, select = c("Date", "Player.ID", "Team", "Team.Id", "Opposition.id", "Venue",
-                                   "Goals", "Shots.On.Target.inc.goals", "Blocked.Shots","Total.Successful.Passes.All",
+sub_data<- subset(data, select = c("Date", "Team", "Opposition", "Venue",
+                                   "Goals", "Goals.Conceded", "Shots.On.Target.inc.goals", "Blocked.Shots","Total.Successful.Passes.All",
                                    "Total.Unsuccessful.Passes.All", "Total.Attempts",
                                    "Key.Passes"))
 
 #storing the file with desired variables
-write.csv("sub_data", "small_soccer_data.csv", row.names = F)
+write.csv(sub_data, "small_soccer_data.csv", row.names = F)
+
+
+#tidying data
+data_tbl <- tbl_df(train.data)
+grouped <- group_by(data_tbl, Date, Team, Opposition, Venue)
+compact.data <- summarise_each(grouped, funs(sum))
+
+#write this new data set into a new file
+write.csv(compact.data, "summarized_data.csv", row.names = F)
 
 
 #####################################################################################################################
 
 #split data set in two
-n <- nrow(sub_data)
-half.point <- round(nrow(sub_data)/2)
+n <- nrow(compact.data)
+half.point <- round(nrow(compact.data)/2)
 
-train.data <- sub_data[1:half.point, ]
-test.data <- sub_data[(half.point+1):n, ]
+train.data <- compact.data[1:half.point, ]
+test.data <- compact.data[(half.point+1):n, ]
 
 
 
