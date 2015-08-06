e2605bb126a4d053199bc3a0d64f328fbd6b93d1
Rose Nyameke <neorzay.01@gmail.com>
2015-08-06 19:28:30 -0400

    correcting goals and creating new vars

--- project_script.R
+++ project_script.R
@@ -1,3 +1,5 @@
+setwd("~/Documents/non-indexed/Stat 139/Final Project/Data/Stat-139-Final-Project/")
+
 #read inital data file
 data <- read.csv("Premier League 2011-12 Match by Match.csv")
 
@@ -11,23 +13,56 @@ data$Total.Attempts <- rowSums(data[, c(64:75)])
 #                                   "Key.Passes"))
 
 
-sub_data <- subset(data, select = c("Date", "Team", "Opposition", "Venue", "Goals", "Goals.Conceded", "Shots.On.Target.inc.goals","Shots.Off.Target.inc.woodwork",
-                                    "Successful.Dribbles", "Unsuccessful.Dribbles", "Through.Ball", "Touches", "Duels.won", "Duels.lost", "Tackles.Won", "Tackles.Lost",
-                                    "Blocked.Shots", "Blocks", "Interceptions", "Recoveries", "Total.Fouls.Conceded", "Total.Fouls.Won", "Yellow.Cards", "Red.Cards",
-                                    "Saves.Made", "Challenge.Lost", "Turnovers", "Dispossessed", "Big.Chances", "Big.Chances.Faced", "Total.Successful.Passes.All",
-                                    "Total.Unsuccessful.Passes.All", "Total.Attempts", "Key.Passes"))
+sub_data <- subset(data, select = c("Date", "Team", "Opposition", "Venue", "Goals", "Goals.Conceded", "Shots.On.Target.inc.goals",
+                                    "Shots.Off.Target.inc.woodwork", "Blocked.Shots", "Total.Successful.Passes.All",
+                                    "Total.Unsuccessful.Passes.All", "Assists", "Key.Passes","Successful.Dribbles",
+                                    "Unsuccessful.Dribbles", "Touches", "Duels.won", "Duels.lost", "Tackles.Won", 
+                                    "Tackles.Lost", "Total.Clearances", "Blocks", "Interceptions", "Recoveries",
+                                    "Total.Fouls.Conceded", "Total.Fouls.Won", "Yellow.Cards", "Red.Cards",
+                                    "Saves.Made", "Challenge.Lost", "Team.Formation", 
+                                    "Turnovers", "Dispossessed", "Big.Chances", "Big.Chances.Faced", "Total.Attempts"))
 
 #storing the file with desired variables
 write.csv(sub_data, "small_soccer_data.csv", row.names = F)
 
 
 #tidying data
+library(dplyr)
+
 data_tbl <- tbl_df(sub_data)
 grouped <- group_by(data_tbl, Date, Team, Opposition, Venue)
 compact.data <- summarise_each(grouped, funs(sum))
 
 #subsetting venue = home because each game is duplicated as home and away
 final.data <- subset(compact.data, Venue == "Home")
+
+#creating chances.created variable
+final.data$Chances.Created <- final.data$Assists + final.data$Key.Passes
+
+#creating chances faced in away data set
+data.away <- subset(compact.data, Venue == "Away")
+data.away$Chances.Faced <- data.away$Assists + data.away$Key.Passes
+
+data.away <- data.away[, c("Date", "Team", "Chances.Faced")]
+names <- names(data.away)
+names[2] <- "Opposition"
+names(data.away) <- names
+
+#including Chances.Faced into final.data
+final.data <- merge(final.data, data.away, by = c("Date", "Opposition"))
+
+#renaming columns
+names <- names(final.data)
+names[6] <- "Shots.On.Target"
+names[7] <- "Shots.Off.Target"
+names[9] <- "Total.Successful.Passes"
+names[10] <- "Total.Unsuccessful.Passes"
+
+
+#Creating new
+final.data$p_o <- final.data$Goals / final.data$Chances.Created
+final.data$p_d <- final.data$Goals.Conceded / final.data$Chances.Faced
+
 
 #renaming columns
 names <- names(final.data)
@@ -38,6 +73,20 @@ names[31] <- "Total.Successful.Passes"
 
 #adding goal difference column
 final.data$Goal.Difference = final.data$Goals - final.data$Goals.Conceded
+
+#reading in file with correct goal numbers
+library(engsoccerdata)
+data(Package = "engsoccerdata2")
+data_rep <- engsoccerdata2
+
+season <- subset(data_rep, Season == 2011 & division == "1", select = c("Date", "home", "visitor", "hgoal", "vgoal"))
+ordered_season <- season[order(season$Date, season$home), ]
+
+#replacement of goal figures
+final.data <- final.data[order(final.data$Date, final.data$Team),]
+
+final.data$Goals <- ordered_season$hgoal
+final.data$Goals.Conceded <- ordered_season$vgoal
 
 #write this new data set into a new file
 write.csv(final.data, "summarized_data.csv", row.names = F)
@@ -54,10 +103,13 @@ test.data <- final.data[(half.point+1):n, ]
 
 
 
+
+
+
+
 #one year's worth of very detailed data, down to the individual. We plan on aggregating this for team stats
 #multiple years' worth of match data, with only information about goals and home vs. away
 
-# for every game, get the goal differential and use that as y variable
 #model on a game to game basis
 
 #run simulations to try to predict and compare to the limited data set
